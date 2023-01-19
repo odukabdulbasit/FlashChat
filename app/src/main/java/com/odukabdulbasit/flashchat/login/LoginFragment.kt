615098bd1e33a778f5c57e3f51fef693c9d02176
Abdulbasit Oduk <a.oduk@pegb.tech>
2023-01-19 17:57:57 +0300

    Created Welcome, register, login and chat screen

---
+++ app/src/main/java/com/odukabdulbasit/flashchat/login/LoginFragment.kt
@@ -0,0 +1,21 @@
+package com.odukabdulbasit.flashchat.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.odukabdulbasit.flashchat.R
+
+
+class LoginFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+}
